ec651d927b819f1ef15b1b5946a8a6a92626a9a0
h0tcat <h0tnori@vivaldi.net>
2020-11-23 22:18:58 +0900

    first commit

---
+++ main.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <ncurses.h>
+#include <unistd.h>
+
+typedef struct
+{
+    int x;
+    int y;
+    char* chara;
+} Canue;
+
+void setup()
+{
+    initscr();
+    noecho();
+    curs_set(0);
+    keypad(stdscr, true);
+    nodelay(stdscr, true);
+    scrollok(stdscr,true);
+}
+
+void generateRock(){
+    int rockX=(rand() % 30)+1;
+    int rockY=20;
+    char* rockChar="*";
+    
+    mvprintw(rockY,rockX,rockChar);
+    scrl(1);
+}
+
+void gameloop(Canue *player)
+{
+    srand((unsigned)time(0UL));
+
+    char position_chr;
+
+    while (1)
+    {
+
+        switch (getch())
+        {
+        case KEY_LEFT:
+            player->x--;
+            break;
+        case KEY_RIGHT:
+            player->x++;
+            break;
+        }
+
+        //プレイヤーの移動制限コード
+        if(player->x<=0)
+            player->x=0;
+        else if(player->x>=30)
+            player->x=30;
+        
+        position_chr=mvinch(player->y,player->x);
+        
+        if(position_chr=='*'){
+            mvprintw(player->y,player->x,"CRASH!");
+            nodelay(stdscr, false);
+            getch();
+            break;
+        }
+        
+
+        //clear();
+        mvprintw(player->y, player->x, player->chara);
+        generateRock();
+        usleep(82000);
+    }
+}
+
+int main(void)
+{
+
+    setup();
+    Canue player = {
+        .chara = "O",
+        .x = 15,
+        .y = 5
+    };
+    gameloop(&player);
+    endwin();
+    return 0;
+}
